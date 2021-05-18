5b5fd774d11c7034544ff880cb977436670b441c
Atsu F. [CTO @ StartonMC] <67171928+xAtsuUC@users.noreply.github.com>
2021-05-17 23:43:59 -0300

    prune.js added and working + permission handler

---
+++ src/commands/prune.ts
@@ -0,0 +1,39 @@
+const { MessageEmbed } = require("discord.js");
+import { Emoji, Message, ReactionUserManager } from "discord.js"
+
+module.exports = {
+    name: "prune",
+    aliases: [],
+    permissions: ['KICK_MEMBERS'],
+    run: async(message: Message, args, client) => {
+        const number = await message.guild.members.prune({
+            dry: true,
+            days: 30
+        })
+        const rusure = new MessageEmbed()
+        .setTitle("Confirmación")
+        .setDescription(`Esto eliminará ${number} miembro(s), estas seguro de que deseas continuar? \nReacciona debajo para confirmar`)
+
+        const m = await message.channel.send(rusure)
+        m.react('❌')
+        m.react('✅')
+
+        m.awaitReactions((reaction, user) => (reaction.emoji.name === '❌' || reaction.emoji.name === '✅') && user.id === message.member.id, { time: 15000, errors: ['time'], max: 1 }).then(async collected => {
+            if(collected?.first().emoji.name === '❌') return message.channel.send(new MessageEmbed().setDescription('❌ Comando cancelado'))
+            if(collected?.first().emoji.name === '✅') { 
+                const suc = await message.channel.send(new MessageEmbed().setDescription('En progreso...'))
+                let count = await message.guild.members.prune({
+                    days: 30,
+                    count: true,
+                    reason: message.member.id + " | " + message.member.displayName
+                })
+                setTimeout(() => {
+                    suc.edit(new MessageEmbed().setDescription(`✅ ${count} miembro(s) han sido eliminados`))
+                }, 5000)
+                m.delete()
+            }
+        }).catch(e => {
+            return m.edit(new MessageEmbed().setDescription('❌ Comando cancelado'))
+        })
+    }
+}
--- src/util/handler/message.ts
+++ src/util/handler/message.ts
@@ -1,16 +1,24 @@
+const { MessageEmbed } = require("discord.js");
 const { prefix } = require('../../storage/settings.json')
 
 module.exports = {
     name: "message",
     run: async(client, message) => {
 
+        const erremb = new MessageEmbed()
+        .setDescription('Whoops, parece que no tienes permiso para hacer esto!')
+        .setColor('RED')
+
         if(message.author.bot) return;
+        if(!message.guild || !message.member ) return;
         const matchedPrefix = prefix
         const args = message.content.slice(matchedPrefix.length).trim().split(/ +/);
         const command = args.shift().toLowerCase();
-        const cmd = client.commands.get(command) || client.commands.find(c => c.aliases?.includes(cmd))
+        const cmd = client.commands.get(command) || client.commands.find(c => c.aliases?.includes(command))
         if(cmd) {
-            cmd.run(message, args, client)
+            if(!cmd.permissions) return cmd.run(message, args, client);
+            if(message.member.permissions.has(cmd.permissions)) return cmd.run(message, args, client);
+            message.reply(erremb)
         }
     }
 }
