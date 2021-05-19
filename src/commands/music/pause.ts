7d0a44eb94ba1a66301a52d978af934d226c0401
Atsu F. [CTO @ StartonMC] <67171928+xAtsuUC@users.noreply.github.com>
2021-05-19 06:16:55 -0300

    pause queue

--- src/commands/music/pause.ts
+++ src/commands/music/pause.ts
@@ -1,11 +1,13 @@
-import { Message, Client } from 'discord.js'
+import { Message, Client, MessageEmbed } from 'discord.js'
 
 
 module.exports = {
     name: "pause",
     aliases: [],
     run: async (client: Client, message: Message, args) => {
-        return;
-
+        if(!message.member.isDJ(message)) return message.reply(client.errors.makeEmbed('No eres un DJ!'))
+        if(!client.distube.isPlaying(message)) return message.reply(client.errors.makeEmbed('Nada reproduciendose!'))
+        client.distube.pause(message)
+        message.reply(new MessageEmbed().setColor("YELLOW").setDescription("Reproducción pausada!"))
     }
 }
