dd72ac886525b7edf33b420e55564ab86b75dc2d
Atsu F. [CTO @ StartonMC] <67171928+xAtsuUC@users.noreply.github.com>
2021-05-19 06:18:45 -0300

    queue status, NEEDS to be better

--- src/commands/music/status.ts
+++ src/commands/music/status.ts
@@ -1,11 +1,45 @@
-import { Message, Client } from 'discord.js'
+import { Message, Client, MessageEmbed } from 'discord.js'
 
 
 module.exports = {
     name: "status",
     aliases: [],
     run: async (client: Client, message: Message, args) => {
-        return;
+        let playornot;
+        let loopstatus;
+        
+        const queue = client.distube.getQueue(message)
+        const ip = client.distube.isPlaying(message)
 
+        switch (queue.repeatMode) {
+            case 0:
+                loopstatus = 'Sin bucle'
+                break;
+            case 1:
+                loopstatus = 'Cancion actual'
+                break;
+            case 2:
+                loopstatus = 'Cola actual'
+                break;
+        }
+
+        switch (ip) {
+            case true:
+                playornot = 'En reproducción'
+                break;
+        
+            case false:
+                playornot = 'Pausada'
+                break;
+        }
+
+        message.reply(
+            new MessageEmbed()
+            .setColor('PURPLE')
+            .setTitle('Estado de la cola')
+            .setDescription('Un poco de info sobre la reproducción de musica actuual!')
+            .addField('Estado:', playornot)
+            .addField('Estado de bucle:', loopstatus)
+        )
     }
 }
