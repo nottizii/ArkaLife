5f6ede0abb1602d496cedd0b38aa0052668d216c
Atsu F. [CTO @ StartonMC] <67171928+xAtsuUC@users.noreply.github.com>
2021-05-19 06:18:28 -0300

    shuffle queue

--- src/commands/music/shuffle.ts
+++ src/commands/music/shuffle.ts
@@ -1,11 +1,16 @@
-import { Message, Client } from 'discord.js'
+import { Message, Client, MessageEmbed } from 'discord.js'
 
 
 module.exports = {
     name: "shuffle",
     aliases: [],
     run: async (client: Client, message: Message, args) => {
-        return;
-
+        if(!client.distube.isPlaying(message)) return message.reply(client.errors.makeEmbed('No hay nada reproduciendo!'));
+        const q = await client.distube.shuffle(message)
+        let songs = q.songs.join('\n')
+        const e = new MessageEmbed()
+        .setTitle("Lista Mezclada!")
+        .setDescription(songs.slice(1000))
+        message.reply(e)
     }
 }
