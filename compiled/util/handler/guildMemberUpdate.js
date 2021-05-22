6e78942f4dec3c1c4ed42abf3fb54d52f4e00458
Atsu F. [CTO @ StartonMC] <67171928+xAtsuUC@users.noreply.github.com>
2021-05-22 06:52:37 -0300

    forgot to commit this :P

--- compiled/util/handler/guildMemberUpdate.js
+++ compiled/util/handler/guildMemberUpdate.js
@@ -1,9 +1,27 @@
 "use strict";
 Object.defineProperty(exports, "__esModule", { value: true });
+const discord_js_1 = require("discord.js");
 let d = new Date();
 module.exports = {
-    name: "guildMemberRemove",
-    run: async (member) => {
-        '';
+    name: "guildMemberUpdate",
+    run: async (oldM, newM) => {
+        if (oldM.roles.cache !== newM.roles.cache)
+            return;
+        let olddiff = oldM.roles.cache.filter(r => !newM.roles.cache.has(r.id));
+        let newdiff = newM.roles.cache.filter(r => !oldM.roles.cache.has(r.id));
+        let oldstr;
+        let newstr;
+        olddiff.forEach(r => oldstr += r.toString());
+        newdiff.forEach(r => newstr += r.toString() + " \n");
+        let embed = new discord_js_1.MessageEmbed()
+            .setTitle("Roles actualizados!")
+            .setDescription(d.toUTCString())
+            .addFields([
+            { name: "Miembro:", value: newM.toString, inline: true },
+            { name: "Antes:", value: oldstr, inline: true },
+            { name: "Ahora:", value: oldstr, inline: true }
+        ])
+            .setThumbnail(newM.user.displayAvatarURL());
+        newM.client.logs?.send(embed);
     }
 };
--- src/util/handler/guildMemberUpdate.ts
+++ src/util/handler/guildMemberUpdate.ts
@@ -2,8 +2,26 @@ import { GuildMember, MessageEmbed } from "discord.js";
 let d = new Date()
 
 module.exports = {
-    name: "guildMemberRemove",
-    run: async (member: GuildMember) => {
-        ''
+    name: "guildMemberUpdate",
+    run: async (oldM: GuildMember, newM: GuildMember) => {
+        if(oldM.roles.cache !== newM.roles.cache) return;
+        let olddiff = oldM.roles.cache.filter(r => !newM.roles.cache.has(r.id))
+        let newdiff = newM.roles.cache.filter(r => !oldM.roles.cache.has(r.id))
+
+        let oldstr;
+        let newstr;
+
+        olddiff.forEach(r => oldstr += r.toString())
+        newdiff.forEach(r => newstr += r.toString() + " \n")
+        let embed = new MessageEmbed()
+        .setTitle("Roles actualizados!")
+        .setDescription(d.toUTCString())
+        .addFields([
+            { name: "Miembro:", value: newM.toString, inline: true},
+            { name: "Antes:", value: oldstr, inline: true},
+            { name: "Ahora:", value: oldstr, inline: true}
+        ])
+        .setThumbnail(newM.user.displayAvatarURL())
+        newM.client.logs?.send(embed)
     }
 }
