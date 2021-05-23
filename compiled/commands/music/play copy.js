29e293b0afe6bc0983939a03869e956b29d06de3
Atsu F. [CTO @ StartonMC] <67171928+xAtsuUC@users.noreply.github.com>
2021-05-23 16:59:42 -0300

    Delete compiled directory

--- compiled/commands/music/play copy.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-module.exports = {
-    name: "play",
-    aliases: [],
-    run: async (client, message, args) => {
-        return;
-    }
-};
